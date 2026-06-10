e7e7d4cd418b1a3a1b7ae8e9c6b01ae8209d81b8
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-06-10 22:08:36 +0200

    Formattato testo

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,25 +4,25 @@ import connection from "../db/connections/connection.js";
 async function index(request, response) {
     try {
         const sql = `
-      SELECT 
-        products.id,
-        products.name,
-        products.price,
-        products.description,
-        products.available,
-        products.in_stock,
-        products.ingredient,
-        products.image,
-        products.created_at,
-        products.updated_at,
-        GROUP_CONCAT(categories.name SEPARATOR ', ') AS categories
-      FROM products
-      LEFT JOIN category_product
-        ON products.id = category_product.product_id
-      LEFT JOIN categories
-        ON category_product.category_id = categories.id
-      GROUP BY products.id
-      ORDER BY products.id
+            SELECT 
+                products.id,
+                products.name,
+                products.price,
+                products.description,
+                products.available,
+                products.in_stock,
+                products.ingredient,
+                products.image,
+                products.created_at,
+                products.updated_at,
+            GROUP_CONCAT(categories.name SEPARATOR ', ') AS categories
+            FROM products
+                LEFT JOIN category_product
+                    ON products.id = category_product.product_id
+                LEFT JOIN categories
+                    ON category_product.category_id = categories.id
+            GROUP BY products.id
+    O       RDER BY products.id
     `;
 
         const [products] = await connection.query(sql);
