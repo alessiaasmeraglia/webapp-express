d6b598873c5e156a81b701b6ee0178476d086fca
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-06-10 16:02:10 +0200

    ADD funzioni CRUD vuote ToDo assegnazione issues

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,3 +1,44 @@
 import { createConnection } from "mysql2";
 import connection from "../db/connections/connection";
 
+async function index(request, response) {
+    try{
+
+    }catch{
+
+    }
+};
+
+async function show(request, response) {
+    try{
+
+    }catch{
+
+    }
+};
+
+async function create(request, response) {
+    try{
+
+    }catch{
+
+    }
+};
+
+async function destroy(request, response) {
+    try{
+
+    }catch{
+
+    }
+};
+
+async function modify(request, response) {
+    try{
+
+    }catch{
+
+    }
+};
+
+export {index, show, create, destroy, modify};
