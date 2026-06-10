cf648b40f17730abc101b0e1849c2efa0949390e
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-06-10 18:08:58 +0200

    Uniti file e mantenuto products controller e router

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -39,4 +39,63 @@ async function index(request, response) {
     }
 }
 
-export { index };
+async function show(request, response) {
+
+    const { id } = request.params;
+
+    const query = `
+        SELECT id, name, price, description, image
+        FROM products p
+        WHERE p.id = ?
+    `;
+
+    try {
+        const [rows] = await connection.execute(query, [id]);
+
+        if (rows.length === 0) {
+            return response.status(404).json({
+                error: 'Prodotto non trovato',
+                results: null
+            });
+        }
+
+        const product = rows[0];
+
+        return response.json({
+            results: product
+        });
+
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({
+            error: 'È successo qualcosa',
+            results: null
+        });
+    }
+}
+
+async function create(request, response) {
+    try{
+
+    }catch{
+
+    }
+};
+
+async function destroy(request, response) {
+    try{
+
+    }catch{
+
+    }
+};
+
+async function modify(request, response) {
+    try{
+
+    }catch{
+
+    }
+};
+
+export {index, show, create, destroy, modify};
--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -1,9 +1,29 @@
 import express from "express";
-import { index } from "../controllers/productsController.js";
+import { 
+    index,
+    show,
+    create,
+    modify,
+    destroy
+} from "../controllers/productsController.js";
 
 const router = express.Router();
 
 // INDEX http://localhost:3000/products
 router.get("/", index);
 
+// SHOW hhtp://localhost:5555/products/:id
+router.get('/:id', show);
+
+// CREATE
+router.post('/', create)
+
+// PATCH
+router.patch('/:id', modify)
+
+// DELETE
+router.delete('/:id', destroy)
+
+
 export default router;
+
